73b4c536f8e59cfeee0305453602148c454df364
Jami Orris <81092462+jamimo76@users.noreply.github.com>
2022-02-09 18:27:22 -0500

    fetch post comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -31,7 +31,7 @@ function RenderCampsite({ campsite }) {
   );
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
   // console.log(comments);
 
   if (comments) {
@@ -56,7 +56,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
             );
           })
         }
-        <CommentForm campsiteId={campsiteId} addComment={addComment}/>
+        <CommentForm campsiteId={campsiteId} postComment={postComment}/>
       </div>
     );
   }
@@ -83,7 +83,7 @@ class CommentForm extends Component {
  
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+    this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
   }
   
   render() {
@@ -185,7 +185,7 @@ function CampsiteInfo(props) {
           <RenderCampsite campsite={props.campsite} />
           <RenderComments 
             comments={props.comments} 
-            addComment={props.addComment}
+            postComment={props.postComment}
             campsiteId={props.campsite.id}
             />
         </div>
